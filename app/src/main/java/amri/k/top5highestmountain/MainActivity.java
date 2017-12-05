17079dda354e9a6a13a62b87c7164d384cd2313a
geoholix <amri.goeholic@gmail.com>
2017-12-05 07:53:53 +0700

    updated with welcome screen

--- app/src/main/java/amri/k/top5highestmountain/MainActivity.java
+++ app/src/main/java/amri/k/top5highestmountain/MainActivity.java
@@ -1,7 +1,9 @@
 package amri.k.top5highestmountain;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartApp(View view) {
+        Intent pindah5 = new Intent(MainActivity.this, number1Activity.class);
+        startActivity(pindah5);
     }
 }
